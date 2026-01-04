0f6843cc5764dcff89b7c2b6f9e9dd22427ae379
mulbumpant-dev <mulbumpant@gmail.com>
2026-01-04 23:05:06 +0000

    Fix Discord logging with file upload

--- netlify/functions/logOrder.js
+++ netlify/functions/logOrder.js
@@ -1,40 +1,37 @@
 const fetch = require('node-fetch');
 const FormData = require('form-data');
-const Busboy = require('busboy'); // to parse incoming multipart/form-data
+const Busboy = require('busboy');
 
 exports.handler = async (event) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const bb = Busboy({ headers: event.headers });
     const fields = {};
-    let fileData = null;
+    let fileBuffer = null;
     let fileName = '';
 
     bb.on('file', (name, file, info) => {
       fileName = info.filename;
       const chunks = [];
-      file.on('data', chunk => chunks.push(chunk));
-      file.on('end', () => {
-        fileData = Buffer.concat(chunks);
-      });
+      file.on('data', (chunk) => chunks.push(chunk));
+      file.on('end', () => { fileBuffer = Buffer.concat(chunks); });
     });
 
-    bb.on('field', (name, val) => {
-      fields[name] = val;
-    });
+    bb.on('field', (name, val) => { fields[name] = val; });
 
     bb.on('close', async () => {
       try {
-        const webhook = "https://discord.com/api/webhooks/YOUR_WEBHOOK";
-
+        const webhook = "https://discordapp.com/api/webhooks/1456986141193670677/HCyuy4Ft76RfwEjbyVCyjxgVMUfHAYzO7laQHEFrU1S3rRqUO3LcpCTf6_whmtkPbZm0";
         const form = new FormData();
+
         form.append("content", `New order received!
 Name: ${fields.name}
 Postcode: ${fields.postcode}
 Address: ${fields.address}
 PLA Color: ${fields.plaColor}
 Total: £${fields.totalCost}`);
-        if (fileData) {
-          form.append("file", fileData, { filename: fileName });
+
+        if (fileBuffer) {
+          form.append("file", fileBuffer, { filename: fileName });
         }
 
         const res = await fetch(webhook, { method: 'POST', body: form });
